e03ccf9b756595bd51c610de77379e45cc3d1068
lychees <lychees67@gmail.com>
2019-09-28 04:44:48 +0800

    Go

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import BootstrapVue from 'bootstrap-vue';
-import App from './App.vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import vueConfig from 'vue-config';
+import VueLazyload from 'vue-lazyload';
+import AsyncComputed from 'vue-async-computed';
+import * as config from '@/config';
+import App from './App';
+import store from './store';
+import router from './router';
+import i18n from './i18n';
 
-Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
 
-const vm = new Vue({
-  render: h => h(App)
+Vue.use(VueLazyload);
+Vue.use(AsyncComputed);
+Vue.use(vueConfig, config);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  i18n,
+  router,
+  components: { App },
+  template: '<App/>',
 });
-
-vm.$mount('#app');
